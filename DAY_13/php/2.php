7e007f3e59bf7df02eda3ebde159332acbce82ac
pprinceppatel <pprinceppatel8383@gmail.com>
2023-07-03 15:38:35 +0530

    day-13 completed

---
+++ DAY_13/php/2.php
@@ -0,0 +1,9 @@
+<?php
+$name = "Prince patel";
+$number = 2101031000167;
+$department = "B.tech IT";
+
+echo "Name: " . $name . "<br>";
+echo "Number: " . $number . "<br>";
+echo "Department: " . $department . "<br>";
+?>
